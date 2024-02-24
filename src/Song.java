4604f4e52a7e3580aa95cf821ca79a91c2863f30
joyceli <joycel2026@student.cis.edu.hk>
2024-02-24 12:02:06 +0800

    :)

--- src/Song.java
+++ src/Song.java
@@ -1,4 +1,28 @@
+import java.util.ArrayList;
 public class Song {
+    private String title;
+    private String artist;
+    private ArrayList<String> names= new ArrayList<>();
 
-    private String
+    public Song(String title, String artist){
+        this.title = title;
+        this.artist = artist;
+    }
+    public String getTitle(){
+        return title;
+    }
+    public String getArtist(){
+        return artist;
+    }
+    public int howMany(ArrayList<String> names){
+        int people = 0;
+        for (String s : names){
+            if (!this.names.contains(s.toLowerCase())){
+                this.names.add(s.toLowerCase());
+                people++;
+            }
+        }
+        return people;
+    }
+
 }
